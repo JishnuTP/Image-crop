3ff15bf687ae51d4bf8ad8397896d1973b0172e2
JishnuTP <tpjishnu5@gmail.com>
2024-08-03 08:28:22 +0530

    backen-url-changed:

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(cors({
-    origin: "https://task-menucard-frontend.vercel.app", // Allow only this origin
+    origin: "https://image-crop-frontend-git-main-jishnutps-projects.vercel.app", // Allow only this origin
     methods: ["GET", "POST", "PUT", "DELETE"], // Allow these HTTP methods
     credentials: true // Allow credentials if needed
   }));
